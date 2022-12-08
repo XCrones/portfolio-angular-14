698d9dba481bb0d056c7f7cfebfe3c35ea3815c8
XCrones <103285327+XCrones@users.noreply.github.com>
2022-12-08 15:54:58 +0300

    feature: add selet projects

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,13 @@ import {
   trigger,
 } from '@angular/animations';
 import { Component, HostListener, OnInit } from '@angular/core';
+import { AboutMeService } from 'src/app/modules/home/services/about-me.service';
+import {
+  HeaderService,
+  IProjects,
+} from 'src/app/services/header/header.service';
 import { NeonService } from 'src/app/services/neon/neon.service';
+import { ShadowService } from 'src/app/services/shadow/shadow.service';
 
 @Component({
   selector: 'app-header',
@@ -34,7 +40,12 @@ import { NeonService } from 'src/app/services/neon/neon.service';
 export class HeaderComponent implements OnInit {
   private _isOpenHamburger = false;
 
-  constructor(private _neonService: NeonService) {}
+  constructor(
+    private _neonService: NeonService,
+    private _headerService: HeaderService,
+    private _aboutmeService: AboutMeService,
+    private _shadowService: ShadowService
+  ) {}
 
   ngOnInit(): void {
     let width = window.innerWidth;
@@ -46,6 +57,24 @@ export class HeaderComponent implements OnInit {
   }
   get hamburger(): boolean {
     return this._isOpenHamburger;
+  }
+  get projects(): IProjects[] {
+    return this._headerService.projects;
+  }
+  get currFramework(): string {
+    return this._aboutmeService.aboutMe.frameWork;
+  }
+
+  isCurrFramework(frameWork: string | undefined): boolean {
+    return !!frameWork ? this.currFramework.includes(frameWork) : false;
+  }
+
+  getlink(project: IProjects): string | undefined {
+    return this.isCurrFramework(project.title) ? undefined : project.link;
+  }
+
+  getColor(frameWork: string): string {
+    return this._shadowService.getColorProject(frameWork);
   }
 
   toggleNeon() {
--- src/app/services/header/header.service.ts
+++ src/app/services/header/header.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { INavItem } from 'src/app/interfaces/nav-item/i-nav-item';
 
+export interface IProjects {
+  title: string;
+  link: string | undefined;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -23,11 +28,30 @@ export class HeaderService {
     },
   ];
 
+  private _projects: IProjects[] = [
+    {
+      title: 'angular',
+      link: 'https://any-dea-angular.web.app',
+    },
+    {
+      title: 'react',
+      link: undefined, //'https://any-dea-react.web.app'
+    },
+    {
+      title: 'vue',
+      link: 'https://anydea-vue.web.app/',
+    },
+  ];
+
   isHide(state?: boolean | undefined): boolean {
     return (this._isHide = state != undefined ? state : this._isHide);
   }
 
   get itemsNav(): Array<INavItem> {
     return this._itemsNav;
+  }
+
+  get projects(): IProjects[] {
+    return this._projects;
   }
 }
