9b4984f2158326272ecd3de07cd766651eddc6e4
XCrones <103285327+XCrones@users.noreply.github.com>
2022-12-27 13:20:56 +0300

    refactor style

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -1,43 +1,24 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { NeonService } from 'src/app/services/neon/neon.service';
 import { HeaderService } from 'src/app/services/header/header.service';
 import { ProjectsService } from './services/projects/projects.service';
+import { IRouterLinkItem, ROUTER_LINKS } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-projects',
-  animations: [
-    trigger('showItems', [
-      state(
-        '*',
-        style({
-          opacity: 0,
-          transform: 'translateX(-50%)',
-        })
-      ),
-      state('source0', style({})),
-      state('source1', style({})),
-      state('source2', style({})),
-      transition('* => source0', [animate('0.7s')]),
-      transition('* => source1', [animate('0.5s')]),
-      transition('* => source2', [animate('0.3s')]),
-    ]),
-  ],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  public projects!: IRouterLinkItem[];
+
   constructor(
     private _projectsService: ProjectsService,
     private _neonService: NeonService,
     private _headerService: HeaderService
-  ) {}
+  ) {
+    this.projects = Object.values(ROUTER_LINKS.projects.children);
+  }
 
   ngOnInit(): void {}
 
@@ -51,4 +32,17 @@ export class ProjectsComponent implements OnInit {
   toggleHideHeader() {
     this._headerService.isHide(!this._headerService.isHide());
   }
+
+  projectIcon = (projectTitle: string) => {
+    switch (projectTitle) {
+      case ROUTER_LINKS.projects.children.todo.title:
+        return 'checklist';
+      case ROUTER_LINKS.projects.children.shop.title:
+        return 'shopping_cart_checkout';
+      case ROUTER_LINKS.projects.children.chat.title:
+        return 'chat';
+      default:
+        return 'menu_book';
+    }
+  };
 }
